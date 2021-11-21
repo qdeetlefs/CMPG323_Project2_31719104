6ad0911a19f2d3411511585694097429d8881897
qdeetlefs <qdeetlefs@gmail.com>
2021-11-21 21:33:51 +0200

    Before Edit and Delete

--- CMPG323_Project2/Models/GalleryDetailModel.cs
+++ CMPG323_Project2/Models/GalleryDetailModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,6 +13,8 @@ namespace CMPG323_Project2.Models
         public DateTime Created { get; set; }
         public string Url { get; set; }
 
+        [DataType(DataType.Text)]
+        [RegularExpression(@"^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$", ErrorMessage = "Coordinates are not valid.")]
         public string Geolocation { get; set; }
 
         public DateTime CapturedDate { get; set; }
